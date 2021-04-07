9bcf9b6dc5078c598c19c39b65540bd7ee4dfa9a
Pedro Tavares <mail@pedrotavares.com>
2021-04-07 18:59:01 +0100

    Updated key-findings and styling

---
+++ src/_data/keyFindings.js
@@ -0,0 +1,59 @@
+const keyFindings = [{
+        percentage: 83,
+        summary: 'Patitent communication was enhanced'
+    },
+    {
+        percentage: 81,
+        summary: 'My patients were more engaged'
+    },
+    {
+        percentage: 89,
+        summary: 'Explaining complex terminology was made easier'
+    },
+    {
+        percentage: 72,
+        summary: 'Use of time during consultation was more efficient'
+    },
+    {
+        percentage: 66,
+        summary: 'Helped in patients with language barriers'
+    },
+    {
+        percentage: 89,
+        summary: 'Improved patient knowledge of their condition'
+    },
+    {
+        percentage: 74,
+        summary: 'Patients have a better understanding of their medications'
+    },
+    {
+        percentage: 85,
+        summary: 'Patients have a better understanding of surgery'
+    },
+    {
+        percentage: 53,
+        summary: 'Improves adherence to medications'
+    },
+    {
+        percentage: 100,
+        summary: 'Would use our illustrative Guide in routine practice'
+    },
+]
+
+module.exports = {
+    homePage: [
+        keyFindings[0],
+        keyFindings[1],
+        keyFindings[2]
+    ],
+    researchPage1: [
+        keyFindings[3],
+        keyFindings[4],
+        keyFindings[5],
+    ],
+    researchPage2: [
+        keyFindings[6],
+        keyFindings[7],
+        keyFindings[8]
+    ]
+}
---
+++ src/_data/supporters.js
@@ -0,0 +1,17 @@
+module.exports = [{
+        name: 'Cook Medical',
+        logo: ''
+    },
+    {
+        name: 'Dr. Falk',
+        logo: ''
+    },
+    {
+        name: 'Janssen',
+        logo: ''
+    },
+    {
+        name: 'Pfizer',
+        logo: ''
+    },
+]
