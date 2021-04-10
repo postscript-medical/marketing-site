c47b3a465fc7b23295596563a86700c684f52a46
Pedro Tavares <mail@pedrotavares.com>
2021-04-10 09:16:31 +0100

    Updates to research and about pages

--- gulp-tasks/sass.js
+++ gulp-tasks/sass.js
@@ -9,7 +9,7 @@ sassProcessor.compiler = require('sass');
 const isProduction = process.env.NODE_ENV === 'production';
 
 // An array of outputs that should be sent over to includes
-const criticalStyles = ['critical.scss', 'home.scss', 'page.scss', 'work-item.scss'];
+const criticalStyles = ['critical.scss', 'home.scss', 'research.scss', 'about-us.scss'];
 
 // Takes the arguments passed by `dest` and determines where the output file goes
 const calculateOutput = ({ history }) => {
@@ -37,11 +37,9 @@ const sass = () => {
         .pipe(sassProcessor().on('error', sassProcessor.logError))
         .pipe(
             cleanCSS(
-                isProduction ?
-                {
+                isProduction ? {
                     level: 2
-                } :
-                {}
+                } : {}
             )
         )
         .pipe(dest(calculateOutput, { sourceMaps: !isProduction }));
--- src/_data/helpers.js
+++ src/_data/helpers.js
@@ -19,5 +19,23 @@ module.exports = {
         }
 
         return response;
+    },
+    /**
+     * Returns the circumference of a circle based on its radius
+     * @param {Number} radius The radius of the circle
+     * @returns {Number} The circumference of the cicle
+     */
+    getCircleCircumference(radius) {
+        // circumference = 2 * PI * radius
+        return 2 * 3.14 * radius;
+    },
+    /**
+     * Returns a partial circumference value based on a given ratio
+     * @param {Number} radius The radius of the circle
+     * @param {Number} ratio The ratio of the full circumference
+     * @returns {Number} The partial of a given circumference
+     */
+    getCircleCircumferencePartial(radius, ratio) {
+        return this.getCircleCircumference(radius) * ratio
     }
 };
