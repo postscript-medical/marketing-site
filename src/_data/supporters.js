e3d28e3fecedd5399192ddde15db402ef542f2bc
Pedro Tavares <mail@pedrotavares.com>
2021-04-14 19:42:49 +0100

    Updated images

--- src/_data/supporters.js
+++ src/_data/supporters.js
@@ -1,17 +1,17 @@
 module.exports = [{
         name: 'Cook Medical',
-        logo: ''
+        logo: 'https://res.cloudinary.com/obscura-limited/image/upload/v1618424561/postscript-medical/marketing-website/supporters/cook-medical_moexuq.jpg'
     },
     {
         name: 'Dr. Falk',
-        logo: ''
+        logo: 'https://res.cloudinary.com/obscura-limited/image/upload/v1618424561/postscript-medical/marketing-website/supporters/dr-falk_imeyjp.jpg'
     },
     {
         name: 'Janssen',
-        logo: ''
+        logo: 'https://res.cloudinary.com/obscura-limited/image/upload/v1618425313/postscript-medical/marketing-website/supporters/janssen_jp3goa.jpg'
     },
     {
         name: 'Pfizer',
-        logo: ''
+        logo: 'https://res.cloudinary.com/obscura-limited/image/upload/v1618424561/postscript-medical/marketing-website/supporters/pfizer_ghuo2b.png'
     },
 ]
