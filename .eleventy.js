7d23974b69defae31f81ebd6ab1a05c35b281e75
Pedro Tavares <mail@pedrotavares.com>
2021-04-17 14:27:22 +0100

    Implemented progressive menu burger

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const getCollectionByDisplayOrder = require('./src/utils/get-collection-by-displ
 module.exports = config => {
     // Set directories to pass through to the dist folder
     config.addPassthroughCopy('./src/images/');
+    config.addPassthroughCopy('./src/js/');
 
     // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
     config.setUseGitIgnore(false);
