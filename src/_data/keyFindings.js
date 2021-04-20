0e0e74c8496fc6746b9b26e7d69461de9967d997
Pedro Tavares <mail@pedrotavares.com>
2021-04-20 21:54:15 +0100

    Add new image to research page

--- src/_data/keyFindings.js
+++ src/_data/keyFindings.js
@@ -47,7 +47,11 @@ const keyFindings = [{
     {
         heading: 'Unanimous positive feedback',
         percentage: 100,
-        summary: 'Would use our illustrative Guide in routine practice'
+        summary: 'Would use our illustrative Guide in routine practice',
+        image: {
+            src: 'https://res.cloudinary.com/obscura-limited/image/upload/v1618951887/postscript-medical/marketing-website/positive-feedback_zvuqdy.jpg',
+            alt: ''
+        }
     },
 ]
 
