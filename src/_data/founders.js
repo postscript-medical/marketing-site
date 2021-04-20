d59289b9a89567cc9e0f64c067bd0f1a2c31749d
Pedro Tavares <mail@pedrotavares.com>
2021-04-20 21:45:39 +0100

    Add founders information

---
+++ src/_data/founders.js
@@ -0,0 +1,11 @@
+module.exports = [{
+        name: 'Graeme Syme',
+        profession: 'Graeme is a Specialist Gastroenterology Dietitian who lives and works in London having trained in Edinburgh. He is passionate about patient care and incorporating the patient within decision making.',
+        avatar_url: 'https://res.cloudinary.com/obscura-limited/image/upload/v1618946207/postscript-medical/marketing-website/graeme_nvzfn8.jpg'
+    },
+    {
+        name: 'Dr Rajan Patel',
+        profession: 'Rajan Patel is a consultant gastroenterologist and general physician. He trained in London and is currently working in New Zealand. He’s got a passion for teaching and innovation.',
+        avatar_url: 'https://res.cloudinary.com/obscura-limited/image/upload/v1618946207/postscript-medical/marketing-website/rajan_x1afig.jpg'
+    }
+];
